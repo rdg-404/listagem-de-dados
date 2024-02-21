707b1d7911ed28fd151a0b5e5a20ab9a9a077aac
rdg-404 <rodrigopaiva0502@gmail.com>
2024-02-20 23:01:44 -0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Plus } from 'lucide-react'
+import { Header } from './components/header'
+import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button variant="primary">
+            <Plus className="size-3" />
+            Create new
+          </Button>
+        </div>
+      </main>
+    </div>
+  )
+}
